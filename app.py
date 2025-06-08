52c4d2cc2f4adda511e7855b8b84a12dbfc5ba98
ErwinRommel14 <sneak.venom@mail.ru>
2025-06-09 01:08:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
